037a949d415c3aec05a78f934377b0751da97618
Tin <25923486+scsfdev@users.noreply.github.com>
2023-05-03 16:40:52 +0800

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FC1_WinForm
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtCOM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(101, 93);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(74, 32);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtCOM
+            // 
+            this.txtCOM.Location = new System.Drawing.Point(101, 57);
+            this.txtCOM.MaxLength = 2;
+            this.txtCOM.Name = "txtCOM";
+            this.txtCOM.Size = new System.Drawing.Size(50, 20);
+            this.txtCOM.TabIndex = 1;
+            this.txtCOM.Text = "7";
+            this.txtCOM.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCOM_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM Port:";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(181, 93);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(74, 32);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Enabled = false;
+            this.btnCapture.Location = new System.Drawing.Point(261, 93);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(74, 32);
+            this.btnCapture.TabIndex = 4;
+            this.btnCapture.Text = "CAPTURE";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(341, 93);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 32);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(421, 93);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 32);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.Location = new System.Drawing.Point(12, 146);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(556, 420);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 7;
+            this.picBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(580, 38);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "FC1 DEMO";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(341, 47);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(74, 32);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Image file is saved into -->";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(580, 578);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCOM);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(596, 616);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FC1 DEMO";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DNWA.PassportAssist;
+using System.IO;
+
+namespace FC1_WinForm
+{
+    public partial class frmMain : Form
+    {
+        PassportReader pr;
+        string imgDir = "";
+        string title = "FC1 Demo";
+
+        int comPort = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pr != null)
+                pr.Stop();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            pr = new PassportReader();
+            pr.DataReceived += new PassportReader.DataReceivedHandler(Pr_DataReceived);
+            pr.ShowProgress += new PassportReader.ShowProgressHandler(Pr_ShowProgress);
+            pr.HideProgress += new PassportReader.HideProgressHandler(Pr_HideProgress);
+
+            imgDir = Properties.Settings.Default.IMAGE_DIR;
+
+            if (!Directory.Exists(imgDir))
+                Directory.CreateDirectory(imgDir);
+
+
+            txtCOM.Text = "7";
+        }
+
+
+        private string GetVersion()
+        {
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetEntryAssembly();
+            return asm.GetName().Version.Major.ToString() + "." + asm.GetName().Version.Minor.ToString() + "." + asm.GetName().Version.Revision.ToString();
+        }
+
+        private void OpenFolder()
+        {
+            var runExplorer = new System.Diagnostics.ProcessStartInfo();
+            runExplorer.FileName = "explorer.exe";
+            runExplorer.Arguments = imgDir;
+            System.Diagnostics.Process.Start(runExplorer);
+        }
+
+        private void Start_Capture()
+        {
+            pr.StartCapture();
+        }
+
+
+
+        private void txtCOM_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string strNum = "0123456789";
+            if (!strNum.Contains(e.KeyChar) && e.KeyChar!= 8)
+                e.Handled = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCOM.Text))
+                return;
+
+            comPort = int.Parse(txtCOM.Text);
+
+            if (comPort <= 0)
+                return;
+
+            pr.Start(comPort);
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnCapture.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (pr.Stop())
+                picBox.Image = null;
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnCapture.Enabled = false;
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            Start_Capture();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            picBox.Image = null;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFolder();
+        }
+
+
+        private bool Pr_HideProgress()
+        {
+            //Console.WriteLine("Hide progress.");
+            return true;
+        }
+
+        private bool Pr_ShowProgress()
+        {
+            //Console.WriteLine("Show progress.");
+            return true;
+        }
+
+        private bool Pr_DataReceived(PassportReaderEventArg e)
+        {
+            if (!string.IsNullOrEmpty(e.Data.PassportNo))
+            {
+                // Passport scanning.
+                string passportInfo = "";
+
+                passportInfo = "Name: " + e.Data.Name + Environment.NewLine +
+                               "Date of Birth: " + e.Data.DateOfBirth + Environment.NewLine +
+                               "Nationality: " + e.Data.Nationality + Environment.NewLine +
+                               "Passport Type: " + e.Data.Type + Environment.NewLine +
+                               "Passport No: " + e.Data.PassportNo.Replace("<", "") + Environment.NewLine +
+                               "Date of Expiry: " + e.Data.DateOfExpiry + Environment.NewLine +
+                               "Issuing Country: " + e.Data.IssuingCountry;
+
+                MessageBox.Show(passportInfo, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else if (!string.IsNullOrEmpty(e.ScanData))
+            {
+                // Barcode scanning.
+                Console.WriteLine("Barcode:" + e.ScanData);
+                MessageBox.Show(e.ScanData, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                e.ScanData = "";
+            }
+
+            if (e.Data.Image != null)
+            {
+                // There is an image, save it.
+
+                string imgFileName = Path.Combine(imgDir, "FC1_" + DateTime.Now.ToString("yyyymmdd_hhmmss") + ".bmp");
+                Image img = e.Data.Image;
+                img.Save(imgFileName);
+                picBox.Image = img;
+            }
+
+            return true;
+        }
+
+        
+    }
+}
